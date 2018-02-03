3a6f63a9eac0cdb697e415b0cb62c40ea809c9b5
Saurabh Rathi <saurabhrathi12@gmail.com>
2018-02-04 04:30:42 +0530

    Ready for demo

---
+++ a.sql
@@ -0,0 +1,3 @@
+create database photocalorie;
+use photocalorie;
+create table consumption( username char(50) not null, fooditem char(50), calories float, fats float, carbohydrates float, proteins float, date date);
