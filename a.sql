87af8691c131d77e66b3396d149854a8033e7091
Saurabh Rathi <saurabhrathi12@gmail.com>
2018-02-05 16:38:36 +0530

    Final App

--- a.sql
+++ a.sql
@@ -3,3 +3,4 @@ use photocalorie;
 create table user(username char(50),password char(150), name char(100));
 create table consumption( username char(50) not null, fooditem char(50), calories float, fats float, carbohydrates float, proteins float, date date);
 create table chat(username char(50), query text, reply text);
+insert into user("saurabhrathi12","qwerty123","Saurabh");
